e08eb426e62add1fccd3d31f29e4282229dc0881
Unknown <n1056722@ntub.edu.tw>
2017-03-09 17:03:09 +0800

    完成咖啡廳 Json解析

--- app/src/main/java/com/example/asuper/json/JsonActivity.java
+++ app/src/main/java/com/example/asuper/json/JsonActivity.java
@@ -2,9 +2,17 @@ package com.example.asuper.json;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,13 +23,14 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public class JsonActivity extends AppCompatActivity {
-private ListView lv;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json);
-        lv= (ListView) findViewById(R.id.lv);
+        lv = (ListView) findViewById(R.id.lv);
+
 
         GitHubService service = GitHubService.retrofit.create(GitHubService.class);
         Call<List<Cafe>> cafe = service.listCafes1();
@@ -30,16 +39,80 @@ private ListView lv;
         cafe.enqueue(new Callback<List<Cafe>>() {
             @Override
             public void onResponse(Call<List<Cafe>> call, Response<List<Cafe>> response) {
-                List<Cafe>data=response.body();
-                lv.setAdapter(new JsonAdapter());
+                List<Cafe> data = response.body();
+
+                lv.setAdapter(new JsonAdapter(data));
             }
 
             @Override
             public void onFailure(Call<List<Cafe>> call, Throwable t) {
-               lv.setText(t.toString());
+                Toast.makeText(JsonActivity.this, "錯誤", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    class Json {
+        String Cafe;
+
+        public Json(String cafe) {
+            this.Cafe = cafe;
+        }
+    }
+
+    private class JsonAdapter extends BaseAdapter {
+        List<Cafe> list;
+
+        public JsonAdapter(List<Cafe> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Holder holder;
+            if (convertView == null) {
+
+                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.json_item, null);
+                holder = new Holder();
+                holder.name = (TextView) convertView.findViewById(R.id.tv_id);
+                holder.assress= (TextView) convertView.findViewById(R.id.tv_ad);
+                holder.tasty= (RatingBar) convertView.findViewById(R.id.rb_tasty);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (Holder) convertView.getTag();
+            }
+
+            holder.name.setText(list.get(position).getName());
+            holder.assress.setText(list.get(position).getAddress());
+            holder.tasty.setRating(list.get(position).getTasty());
+
+
+
+            return convertView;
+        }
+
+        class Holder {
+            TextView assress;
+            TextView  name;
+            RatingBar tasty;
+
+
+        }
+    }
 
 }
